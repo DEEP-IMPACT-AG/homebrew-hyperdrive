e89cfb61bf96940ad361ac82fcc264ea4f86f765
codebuild <info@deep-impact.ch>
2018-06-30 17:21:23 +0200

    Brew formula update for skuld version v0.6.0

--- skuld.rb
+++ skuld.rb
@@ -1,9 +1,9 @@
 class Skuld < Formula
   desc "CLI tool for AWS MFA credentials"
   homepage "https://github.com/DEEP-IMPACT-AG/skuld"
-  url "https://github.com/DEEP-IMPACT-AG/skuld/releases/download/v0.5.0/skuld_0.5.0_darwin_amd64.tar.gz"
-  version "0.5.0"
-  sha256 "232a74e93336cb4d9eb01a5f4df4fbb3600bdff88fcf89a84214518b66f04748"
+  url "https://github.com/DEEP-IMPACT-AG/skuld/releases/download/v0.6.0/skuld_0.6.0_darwin_amd64.tar.gz"
+  version "0.6.0"
+  sha256 "34791c8dab5d2163db83063794aabcde07187da5bd68769eee68e9c049aaf739"
 
   def install
     bin.install "skuld"
